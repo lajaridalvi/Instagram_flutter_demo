b9093b9669af2c83484dd1abc9d368c7a2f0b18b
unknown <lajari@binarynumbers.io>
2018-10-17 15:34:14 +0530

    first page gui done

---
+++ lib/insta_list.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_flutter/insta_story.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class InstaList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var deviceSize = MediaQuery.of(context).size;
+    return new ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) => index == 0
+          ? new SizedBox(
+              child: new InstaStory(),
+              height: deviceSize.height * 0.20,
+            )
+           : Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          new Container(
+                            height: 40.0,
+                            width: 40.0,
+                            decoration: new BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: new DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: new NetworkImage( "https://articlebio.com/uploads/bio/2016/02/15/nick-bateman.jpg"
+                          ),
+                        ),
+                      ),
+                    ),
+                      new SizedBox(
+                        width: 10.0,
+                      ),
+                      new Text(
+                        "Nich Batemen",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )
+                  ],
+                      ),
+                new IconButton(
+                  icon:Icon(Icons.more_vert),
+                  onPressed: null,
+                )
+                ],
+                    ),
+                  
+                ),
+            
+           Flexible(
+                  fit: FlexFit.loose,
+                  child: new Image.network(
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkHxm_-AMIyN-FbY0N0zIgNmTadO6Y6DHdz_Gud2prX_I42aXV",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+
+             Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          new Icon(
+                            FontAwesomeIcons.heart,
+                          ),
+                          new SizedBox(
+                            width: 16.0,
+                          ),
+                          new Icon(
+                            FontAwesomeIcons.comment,
+                          ),
+                          new SizedBox(
+                            width: 16.0,
+                          ),
+                          new Icon(FontAwesomeIcons.paperPlane),
+                        ],
+                      ),
+                      new Icon(FontAwesomeIcons.bookmark)
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    "Liked by pawankumar, pk and 528,331 others",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      new Container(
+                        height: 40.0,
+                        width: 40.0,
+                        decoration: new BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: new DecorationImage(
+                              fit: BoxFit.fill,
+                              image: new NetworkImage(
+                                  "https://pbs.twimg.com/profile_images/916384996092448768/PF1TSFOE_400x400.jpg")),
+                        ),
+                      ),
+                      new SizedBox(
+                        width: 10.0,
+                      ),
+                      Expanded(
+                        child: new TextField(
+                          decoration: new InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Add a comment...",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child:
+                      Text("1 Day Ago", style: TextStyle(color: Colors.grey)),
+                )
+              ],
+            ),
+    );
+  }
+}
